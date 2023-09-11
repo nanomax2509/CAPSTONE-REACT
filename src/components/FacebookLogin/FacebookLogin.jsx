63730d76ad2a97056156a7a3ceb05f8be5f0e6b8
QuocAnh <nguyenquocanh2509@gmail.com>
2023-09-11 17:37:58 +0700

    fix code fb

--- src/components/FacebookLogin/FacebookLogin.jsx
+++ src/components/FacebookLogin/FacebookLogin.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import axios from 'axios';
 import FacebookLogin from 'react-facebook-login'
 export default function facebookLogin() {
-    const responseFacebook = (responsive)=>{
-        console.log(responsive);
-    }
+    const responseFacebook = (response) => {
+        axios({
+          url: "https://shop.cyberlearn.vn/api/Users/facebooklogin",
+          method: "post",
+          data: {
+            facebookToken: response.accessToken,
+          },
+        }).then((res) => {
+          //Lưu vào localstorage
+          localStorage.setItem("accessToken", res.data.content.accessToken);
+        });
+      };
   return (
     <div>
         <FacebookLogin
