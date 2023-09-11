349d3ccb56e8afbffe1f7ddf6a8ba987560f8069
Kiradev <kira.hoangphong@gmail.com>
2023-09-11 22:58:05 +0700

    Ghi chú về commit

--- src/components/ListProduct/ListProduct.jsx
+++ src/components/ListProduct/ListProduct.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CardProduct from '../CardProduct/CardProduct';
-
-// import kiểu module.
 import css from './ListProduct.module.scss';
 import { mergeClassName } from '../../utils';
-
-// SOLID:
-// Single-responsibility principle: Đảm nhiệm một nhiệm vụ duy nhất
-
-// nhiệm vụ: render list danh sách sản phẩm
-// ( X ): Không có nhiệm vụ show ra UI làm sao hết - vd như margin-bottom.
 function ListProduct(props) {
 	const { listProduct, style, className } = props;
-
-	// <div className={css.ListProduct}> ( X )
 	return (
 		<div className={mergeClassName(css.ListProduct, className)} style={style}>
 			{Array.isArray(listProduct) &&
